72b7fde5d00e2d31ea5276051658f8c7af530eef
Jessica Gihm <jessica,gihm@gmail.com>
2023-07-26 11:39:06 -0700

    completed Nav

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -17,11 +17,52 @@ function Nav() {
             <li className="nav-item">
                 <NavLink className="nav-link" to="/">HOME</NavLink>
             </li>
+
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Inventory
+                  </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <NavLink className="dropdown-item" to="/automobiles">
+                      Automobile List
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/automobiles/create">
+                      Create an Automobile
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/manufacturers">
+                      Manufacturer List
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/manufacturers/create">
+                      Create a Manufacturer
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/models">
+                      Models List
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/models/create">
+                      Create a Model
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+
+
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Service
                   </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+
                   <li>
                     <NavLink className="dropdown-item" to="/technician/create">
                       Add a Technician
@@ -49,6 +90,52 @@ function Nav() {
                   </li>
                 </ul>
               </li>
+
+
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Sales
+                  </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <NavLink className="dropdown-item" to="/salespeople">
+                      Salespeople List
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/salespeople/create">
+                      Add a Salesperson
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/customers">
+                      Customer List
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/customers/create">
+                      Add a Customer
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/sales">
+                      List of Sales
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/sales/create">
+                      Create New Sale
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/sales/history">
+                      Sales History
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+
+
           </ul>
         </div>
       </div>
